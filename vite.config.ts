0f1aa35ad11958845f42d97c1cb842ca88813a97
Guillaume Lessard <guillaume@qector.store>
2026-07-02 10:42:58 -0400

    deploy: compile project to docs folder and configure CNAME at root

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
